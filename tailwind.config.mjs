3e8b325dd6a40adda4b49de9797d1f50d772011d
orlancst <orlancssst@gmail.com>
2024-01-08 23:07:26 -0500

    adding new stuff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+
 	theme: {
 		extend: {
 			fontFamily: {
 				"montserrat": ["Montserrat, sans-serif"],
 			},
+			backgroundImage: {
+				"heroBackground": "url('/src/assets/imgs/car1.jpg')",
+			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated'),
+		require('flowbite/plugin')
+	],
 }
